bbe5206651a37f40a3a263dc23e820cc34a3d6e0
sweissooc2022 <114678287+sweissooc2022@users.noreply.github.com>
2023-03-14 20:57:44 +0000

    Initial commit

---
+++ src/bankdb/BankDB.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bankdb;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Sam
+ */
+public class BankDB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        DBConnector dbconn = new DBConnector();
+        dbconn.createDB();
+        
+    }
+    
+}
---
+++ src/bankdb/DBConnector.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bankdb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Sam
+ */
+public class DBConnector {
+    // final = constant, cannot change
+    private final String DB_URL = "jdbc:mysql://localhost";
+    private final String USER = "pooa";
+    private final String PASSWD = "pooa";
+    
+    public void createDB() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWD);
+            Statement stmt = conn.createStatement();
+            stmt.execute("CREATE DATABASE bank");
+            System.out.println("DB Created");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    String dbName = "hospital";
+//    String DB_URL = "jdbc:mysql://localhost/" + dbName;
+//    String USER = "ooc2022";
+//    String PASS = "ooc2022";  
+    
+//                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+//            Statement stmt = conn.createStatement();
+//            stmt.execute("USE hospital;");
+}
